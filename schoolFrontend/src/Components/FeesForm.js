3f1490060ab6187c9c4cc33aff1c9ae19f684df2
yash <test@gmail.com>
2023-03-29 06:42:03 -0700

    fees Calculation form completed on change the check box

--- schoolFrontend/src/Components/FeesForm.js
+++ schoolFrontend/src/Components/FeesForm.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Form, useActionData, redirect } from "react-router-dom";
+import classes from './FeesForm.module.css';
+import SelectBoxFinancialYear from './SelectBoxFinancialYear';
 function FeesForm() {
+
+
     const fees = {
-        financialYear: "2022 - 23",
+        financialYear: "2022-23",
+        monthlyFees: 100,
+        admissionFees: 500,
+        examFees: 50,
         feesDetail: [
             [{
                 id: "212345_1",
@@ -10,7 +17,7 @@ function FeesForm() {
                 amount: 20.32,
                 dt: "10-10-23",
                 userId: "123456",
-                status: "SUBMITTED"
+                status: "PENDING"
             },
             {
                 id: "212345_2",
@@ -18,7 +25,7 @@ function FeesForm() {
                 amount: 100,
                 dt: "10-10-23",
                 userId: "123456",
-                status: "SUBMITTED"
+                status: "PENDING"
             },
             {
                 id: "212345_3",
@@ -103,7 +110,57 @@ function FeesForm() {
         ]
 
     };
-    // const lengthOfArray = fees.feesDetail.length;
+    const [total, setTotal] = useState(0);
+    const [monthlyChecked, setMonthlyChecked] = useState(0);
+    const [admissionChecked, setAdmissionChecked] = useState(0);
+    const [examChecked, setExamChecked] = useState(0);
+    const isAdmissionFeesSubmitted = fees.feesDetail.map(arr => arr.findIndex(obj => obj.feesFor === 'ADMISSION' && obj.status === 'SUBMITTED')).findIndex(obj => obj === 0) != -1 ? true : false;
+    const [monthyTotal, setMonthlyTotal] = useState(0);
+    const [admissionTotal, setAdmissionTotal] = useState(!isAdmissionFeesSubmitted ? fees.admissionFees : 0);
+    const [examTotal, setExamTotal] = useState(0);
+    const [selectBoxFinancialYearValue, setSelectBoxFinancialYearValue] = useState('');
+
+    
+
+    useEffect(() => {
+        setTotal(Number(monthyTotal) + Number(examTotal) + Number(admissionTotal));
+    }, [monthyTotal, examTotal, admissionTotal])
+
+    useEffect(() => {
+        setMonthlyTotal(fees.monthlyFees * monthlyChecked);
+    }, [monthlyChecked]);
+    useEffect(() => {
+        setExamTotal(fees.examFees * examChecked);
+    }, [examChecked]);
+
+    const checkBoxOnChange = (event,value)=>{
+        if (value === 'ADMISSION') {
+            if (event.target.checked) {
+                setAdmissionChecked(monthlyChecked + 1);
+            } else {
+                setAdmissionChecked(monthlyChecked - 1);
+            }
+            
+        }else if (value === 'EXAM') {
+            if (event.target.checked) {
+                setExamChecked(examChecked + 1);
+            } else {
+                setExamChecked(examChecked - 1);
+            }
+            setExamTotal(examChecked * fees.examFees)
+        }else{
+            if (event.target.checked) {
+                setMonthlyChecked(monthlyChecked + 1);
+                
+            } else {
+                setMonthlyChecked(monthlyChecked - 1);
+            }
+            setMonthlyTotal(monthlyChecked * fees.monthlyFees);
+        }
+    }
+    const getSelectBoxFinancialYear = (value) => {
+        setSelectBoxFinancialYearValue(fees.financialYear);
+    }
     return (
         <>
         <div class="card " style={{ margin: '2rem' }}>
@@ -112,6 +169,7 @@ function FeesForm() {
             </div>
             <div class="card-body">
                 <Form method="POST" >
+
                     <div class="form-check">
                         {fees.feesDetail.map((feesDetails, index) =>
                             <div className={'card-group'}>
@@ -119,12 +177,18 @@ function FeesForm() {
                                     <div class="card">
                                         <div className={`card-body  ${f.status === 'PENDING' ? "text-warning" : "text-success"}`}>
                                             <h5 class="card-title">{f.feesFor}</h5>
-                                            <p class="card-text">Amount : {f.amount}</p>
-
-                                            <input class="form-check-input" type="checkbox" value="" id={f.id} disabled={f.status === 'PENDING' ? false : true} />
-                                            <label class="form-check-label" for={f.id}>
-                                                {f.status === 'PENDING' ? "Select for Submit Fees" : "Submitted"}
-                                            </label>
+                                            <p class="cardthis-text">Amount : {f.amount}</p>
+                                            <div className={classes.frmCheckbox}>
+                                                <input class="form-check-input" type="checkbox" value={f.amount}
+                                                    id={f.id}
+                                                    disabled={f.status === 'PENDING' ? false : true}
+                                                    defaultChecked={f.status === 'PENDING' ? false : true}
+                                                    onChange={event => checkBoxOnChange(event,f.feesFor)}
+                                                />
+                                                <label class="form-check-label" for={f.id}>
+                                                    {f.status === 'PENDING' ? "Select for Submit Fees" : "Submitted"}
+                                                </label>
+                                            </div>
                                         </div>
                                         <div class="card-footer">
                                             <small class="text-muted">{f.dt ? f.dt : 'Pending'}</small>
@@ -134,6 +198,130 @@ function FeesForm() {
                                 )}
                             </div>)}
 
+                    </div>
+
+                    <div class="card " style={{ width: '50rem' }}>
+                        <div class="card-body">
+                            <div class="form-row">
+                            <div class="row">
+                            <div class ="col-3">
+                            <label htmlFor="financeYr">Finance Year</label>
+                            <SelectBoxFinancialYear id = "financeYr" defaultValue = {fees.financialYear} getSelectBoxFinancialYear = {getSelectBoxFinancialYear}/> 
+                            </div>
+                            </div>
+                                {<div class="row">
+                                    <div class="form-group  col" >
+                                        <label htmlFor="admissionFees">Admission Fees</label>
+                                        <input type="number"
+                                            pattern="[0-9]*"
+                                            id="admissionFees"
+                                            name="admissionFees"
+                                            readOnly={true}
+                                            className={`form-control`}
+                                            defaultValue={fees.admissionFees}
+                                            aria-describedby="inputGroupPrepend3"
+                                        />
+                                    </div>
+
+
+                                    <div class="form-group col-5">
+                                        <label htmlFor="totalAdmissionFees">Total Admission Fees</label>
+                                        <input type="number"
+                                            pattern="[0-9]*"
+                                            id="totalAdmissionFees"
+                                            name="totalAdmissionFees"
+                                            defaultValue={fees.admissionFees}
+                                            class="form-control"
+                                            readOnly={true}
+                                            onChange={(event) => setAdmissionTotal(event.target.value)} />
+                                    </div>
+                                </div>
+                                }
+                                <div class="row">
+                                    <div class="form-group  col-3" >
+                                        <label htmlFor="monthlyFees">Monthly Fees</label>
+                                        <input type="number"
+                                            pattern="[0-9]*"
+                                             id="monthlyFees"
+                                            name="monthlyFees"
+                                            readOnly={true}
+                                            className={`form-control`}
+                                            defaultValue={fees.monthlyFees}
+                                            aria-describedby="inputGroupPrepend3"
+                                        />
+                                    </div>
+                                    <div class="form-group col ">
+                                        <label htmlFor="monthCount">Month Count</label>
+                                        <input type="number"
+                                            pattern="[0-9]*"
+                                             id="monthCount" 
+                                             name="monthCount"
+                                            class="form-control"
+                                            value={monthlyChecked}
+                                            />
+                                    </div>
+                                    <div class="form-group  col">
+                                        <label htmlFor="totalMonthlyFees">Total Monthly Fees</label>
+                                        <input type="number"
+                                            pattern="[0-9]*" id="totalMonthlyFees" name="totalMonthlyFees"
+                                            defaultValue={0}
+                                            value={monthyTotal}
+                                            readOnly={true}
+                                            class="form-control" onChange={(event) => setMonthlyTotal(event.target.value)}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group  col-3" >
+                                        <label htmlFor="examFees">Exam Fees</label>
+                                        <input type="text" id="examFees"
+                                            name="examFees"
+                                            readOnly={true}
+                                            className={`form-control`}
+                                            aria-describedby="inputGroupPrepend3"
+                                            defaultValue={fees.examFees}
+                                        />
+                                    </div>
+                                    <div class="form-group col ">
+                                        <label htmlFor="examCount">Exam Count</label>
+                                        <input type="number"
+                                            pattern="[0-9]*" 
+                                        id="examCount"
+                                         name="examCount"
+                                            class="form-control"
+                                            value= {examChecked}
+                                           
+                                        />
+                                    </div>
+                                    <div class="form-group  col">
+                                        <label htmlFor="totalExamFees">Total Exam Fees</label>
+                                        <input type="number"
+                                            pattern="[0-9]*"
+                                            id="totalExamFees"
+                                            name="totalExamFees"
+                                            defaultValue={0}
+                                            value={examTotal}
+                                            class="form-control"
+                                            readOnly={true}
+                                            onChange={(event) => setExamTotal(event.target.value)}
+
+                                        />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-group  col-3" >
+                                        <h4> Grand Total </h4>
+                                    </div>
+                                    <div class="form-group col ">
+
+                                    </div>
+                                    <div class="form-group  col">
+                                        <h4>  {total} </h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </Form>
             </div>
---
+++ schoolFrontend/src/Components/SelectBoxFinancialYear.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import Form from 'react-bootstrap/Form';
+
+function SelectBoxFinancialYear(props) {
+    const onChange = (event) => {
+        const value = event.target.value;
+        props.getSelectBoxFinancialYear(value);
+    };
+
+    return (
+        <>
+        <Form.Select aria-label="Default select example"
+            onChange={onChange} name='financeYr' defaultValue = {props.defaultValue}>
+            <option value="" >Select Financial Year</option>
+            <option value="2021-22">2021-22</option>
+            <option value="2022-23">2022-23</option>
+            <option value="2022-24">2022-24</option>
+            <option value="2024-25">2024-25</option>
+            <option value="2025-26">2025-26</option>
+            
+        </Form.Select>
+        </>
+    );
+}
+export default SelectBoxFinancialYear;
